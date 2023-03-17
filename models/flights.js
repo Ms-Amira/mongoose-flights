3d41b813f5afd6b9f568cb2a18d999524837f4c9
Mimi <trish21215@yahoo.com>
2023-03-16 23:34:59 -0400

    Build skeleton

--- models/flights.js
+++ models/flights.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const flightsSchema = new mongoose.Schema({
-    airline: {
-        type: String,
-    enum: []}, 
-    airport: String,
-    flightNo: Number,
+    airline: { type: String,
+    enum: ['American', 'Southwest', 'United', 'Delta']}, 
+    airport: { String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
+    flightNo: {
+        Number, min: 10, max: 9999},
     departs: Date
 })
+
+module.exports.mongoose.model('Flight', flightsSchema);
